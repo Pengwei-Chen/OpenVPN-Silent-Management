f77490ec25784fe37a2936278ed2dc24d9477363
Pengwei Chen <61215150+Pengwei-Chen@users.noreply.github.com>
2022-09-22 16:03:43 +0800

    Add files via upload

---
+++ Connect.py
@@ -0,0 +1,2 @@
+import os
+os.popen(r'"C:\Program Files\OpenVPN\bin\openvpn-gui.exe" --connect 89.187.178.94_udp.ovpn').readlines()
---
+++ Disconnect.py
@@ -0,0 +1,2 @@
+import os
+os.popen(r'"C:\Program Files\OpenVPN\bin\openvpn-gui.exe" --command disconnect_all').readlines()
